0f41ba208f9efc4591f8bc8bfc465bbd80b02c47
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2020-09-19 11:27:38 +0530

    entrypoint-heroku: No more user switching

--- bin/entrypoint-heroku.sh
+++ bin/entrypoint-heroku.sh
@@ -5,7 +5,6 @@ set -e
 exec \
     gunicorn subscriber.asgi:application \
         --name=subscriber \
-        --user=$APP_USER --group=$APP_USER \
         --bind=0.0.0.0:$PORT \
         --log-level=$LOG_LEVEL \
         --log-file=- \
