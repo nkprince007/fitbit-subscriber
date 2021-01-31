ca5112ee8d0e0b1dcc84216e7cc3f594372f47e4
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2021-01-31 20:21:13 +0530

    fitbit_data: Add APIs for areachart

--- fitbit_data/urls.py
+++ fitbit_data/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from fitbit_data.views import get_activity_summary, get_patient_details
+from fitbit_data.views import get_activity_summary, get_activity_zones, get_patient_details
 
 
 urlpatterns = [
     path('activity_summary/', get_activity_summary, name='activity_summary'),
     path('patient_details/', get_patient_details, name='patient_details'),
+    path('activity_zones/', get_activity_zones, name='activity_zones'),
 ]
--- fitbit_data/views.py
+++ fitbit_data/views.py
@@ -1,10 +1,12 @@
-from fitbit_data.exceptions import InvalidPatientIdException, InvalidPeriodException
+from datetime import timedelta, datetime
 from random import randint
 
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from fitbit_data.exceptions import InvalidPatientIdException, InvalidPeriodException
 
 
 def dashboard(request):
@@ -45,4 +47,30 @@ def get_activity_summary(request):
         }
         for i in range(num_weeks)
         for week_day in week_days
+    ])
+
+
+@api_view(('POST',))
+def get_activity_zones(request):
+    period = request.data.get('period')
+    try:
+        period = int(period)
+    except (ValueError, TypeError):
+        raise InvalidPeriodException()
+
+    patient_id = request.data.get('patientId')
+    try:
+        patient_id = int(patient_id)
+    except (ValueError, TypeError):
+        raise InvalidPatientIdException()
+
+    return Response([
+        {
+            'date': (datetime.today() - timedelta(i)).strftime('%d/%m/%Y'),
+            'Sedentary': randint(0, 100),
+            'Lightly active': randint(0, 100),
+            'Fairly active': randint(0, 100),
+            'Very active': randint(0, 100),
+        }
+        for i in range(period)
     ])
